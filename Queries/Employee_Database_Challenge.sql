270c62db5810e431856cb07a162427f5f3379ab4
JerryMcG <76828978+JerryMcG@users.noreply.github.com>
2021-08-14 16:31:32 -0400

    deliv1+2 complete

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -1,4 +1,11 @@
-select e.emp_no, e.first_name, e.last_name, t.title, t.from_date, t.to_date
+--------------------------------------------------
+--deliverable 1--
+select e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	t.title, 
+	t.from_date, 
+	t.to_date
 into retirement_titles
 from employees as e
 inner join titles as t on (e.emp_no = t.emp_no)
@@ -7,16 +14,30 @@ order by e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+	first_name,
+	last_name,
+	title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
 -- number of employees retiring by title
-select count(title) as total, title
+select count(title) as total, 
+	title
 into retiring_titles
 from unique_titles
 group by title
 order by total desc; 
+
+--------------------------------------------------
+--deliverable 2--
+select distinct on (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date,de.from_date, de.to_date,t.title
+into mentor_elig
+from employees as e
+inner join dept_emp as de on (e.emp_no = de.emp_no)
+inner join titles as t on (e.emp_no = t.emp_no)
+where de.to_date = ('9999-01-01')
+and (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+order by e.emp_no;
+
+
